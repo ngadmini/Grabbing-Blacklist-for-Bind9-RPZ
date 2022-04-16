7775b3b7fea33f5ad563cb7c7822afa51c117d34
nGadimin <pragola1pati@gmail.com>
2022-04-17 00:23:23 +0700

    tidy-up.commit

--- libs/grab_lib.sh
+++ libs/grab_lib.sh
@@ -161,7 +161,7 @@ f_syn() {   # passwordless ssh for "backUP oldDB and rsync newDB"
             printf "[INFO] use \x1b[92m'shutdown -c'\x1b[0m at host: %s to abort\n" "$HOST"
 
             # OR comment 3 lines above AND uncomment 2 lines below, if you have sufficient RAM
-   			#    DON'T add space after "#". it's use by this script at line 78
+            #    DON'T add space after "#". it's use by this script at line 78
             #printf "Reload BIND9-server:%s\n" "$HOST"
             #ssh root@"$HOST" "rndc reload"
          fi
