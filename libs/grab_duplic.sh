9778c14e85c87960d8a4f292a93f2a5a764380dc
ngadmini <pragola1pati@gmail.com>
2022-06-11 19:00:22 +0700

    v7.0-commit

--- libs/grab_duplic.sh
+++ libs/grab_duplic.sh
@@ -18,8 +18,7 @@ cd "${_DIR}"
 readonly _LIB="${_DIR}"/grab_library
 if [[ -e ${_LIB} ]]; then
    if [[ $(stat -L -c "%a" "${_LIB}") != 644 ]]; then chmod 644 "${_LIB}"; fi
-   source "${_LIB}"; f_trap
-#; f_cnf
+   source "${_LIB}"; f_trap; f_cnf
 else
    printf "[FAIL] %s notFOUND\n" "${_LIB##*/}"; exit 1
 fi
