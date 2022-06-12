4f298e224db38f719b0187e96e656e8c7861c2be
ngadmini <pragola1pati@gmail.com>
2022-06-13 02:32:18 +0700

    v7.0-commit

--- libs/grab_rsync.sh
+++ libs/grab_rsync.sh
@@ -10,7 +10,7 @@
 # shellcheck source=/dev/null disable=SC2059 disable=SC2154
 
 T=$(date +%s%N)
-set -Eeuo pipefail; shopt -s lastpipe
+set -Eeuo pipefail
 PATH=/usr/local/bin:/usr/bin:/bin:$PATH
 _DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "${_DIR}"
