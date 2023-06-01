6f825fe60c63f408dcffc7c5311a7da9c1887276
ngadmini <pragola1pati@gmail.com>
2023-06-02 04:27:32 +0700

    v9.9-commit

--- libs/grab_http.sh
+++ libs/grab_http.sh
@@ -126,9 +126,10 @@ trust=$(mktemp -p "${_DIR}")
 untrust=$(mktemp -p "${_DIR}")
 porn=$(mktemp -p "${_DIR}")
 f_sm6 1 "${ar_uri[1]}"; f_do     # add gambling-domains to trust+ category
-f_sm6 7 "${ar_uri[7]}"           # error-code curl: (35) & firefox: PR_CONNECT_RESET_ERROR
-#f_add "${ar_url[7]}" | _sed -e "${ar_reg[0]}" -e "${ar_reg[3]}" > "${trust}"; f_do
-_sed -e "${ar_reg[0]}" ~/Downloads/domains_isp | _sed -e "${ar_reg[3]}" > "${trust}"; f_do
+f_sm6 7 "${ar_uri[7]}"
+# frequently error-code curl: (35) & firefox: PR_CONNECT_RESET_ERROR
+f_add "${ar_url[7]}" | _sed -e "${ar_reg[0]}" -e "${ar_reg[3]}" > "${trust}"; f_do
+#_sed -e "${ar_reg[0]}" ~/Downloads/domains_isp | _sed -e "${ar_reg[3]}" > "${trust}"; f_do
 # reducing adult entries then moving it's to adult category
 printf "%12s: %-66s" "reducing" "porn domains and move it's to ${ar_cat[0]^^} CATEGORY"
 f_add "${ar_url[19]}" | _sed -e "${ar_reg[0]}" > "${porn}"   # use it's as a control to reducing
