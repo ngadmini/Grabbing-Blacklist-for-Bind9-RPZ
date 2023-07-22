4cf6a4370a249a73e16cc8b4a8427c65af7e4654
ngadmini <pragola1pati@gmail.com>
2023-07-23 00:15:49 +0700

    v10.1-commit

--- libs/grab_http.sh
+++ libs/grab_http.sh
@@ -89,7 +89,7 @@ f_ok
 # check the actual longest of TLDs
 printf "${_pre} %-63s" "check the actual longest of Top Level Domains"
 _act=$(curl -s "${_tld}" | _sed '/#/d;s/[A-Z]/\L&/g' | awk '{print length}' | _srt -g | tail -1)
-_reg=$(head -1 "${ar_shn[0]}" | awk -F'|' '{print $7}')
+_reg=$(echo "${ar_reg[0]}" | awk -F'|' '{print $6}')
 _cur=$(echo "${_reg}" | cut -d\{ -f2 | cut -d, -f1)
 _neo=$(("$_act"+1))
 
